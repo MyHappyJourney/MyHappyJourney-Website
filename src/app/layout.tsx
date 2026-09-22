3fcf5c60ee6e95e50391798e923b1067b9a62847
MyHappyJourney <Karthik@myhappyjourney.com>
2026-09-22 16:34:13 +0530

    fix(analytics): update Meta Pixel ID
    Replace the outdated Meta Pixel ID with the correct production ID to ensure accurate tracking.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=4451237028424324&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=4621313678114939&ev=PageView&noscript=1"
           />
         </noscript>
         <Script id="meta-pixel" strategy="afterInteractive">
@@ -42,7 +42,7 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window,document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '4451237028424324');
+fbq('init', '4621313678114939');
 fbq('track', 'PageView');`}
         </Script>
         <Script id="gtm-script" strategy="afterInteractive">
